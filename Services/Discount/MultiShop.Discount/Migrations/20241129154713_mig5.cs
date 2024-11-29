6e2c139ed9ee7364d4039c4aa09aca7c58675119
Muhammet Akyol <akyuzakyuzmuhammed@gmail.com>
2024-11-29 19:07:34 +0300

    Discount Mikroservisi Kurulumu. Manage Nuget Packages(EntityFrameworkCore,Design,SqlServer,Tools) Kurulumları. Context'in Oluşturulması. Veri Tabanının Migrations(Kurulum) Yapılması. Dto ve Servislerin Oluşturulması. İndirim Kuponu Servisi Tanımlama ve Servis Metotlarının Tamamlanması. Discount Api'sini Oluşturma ve Testi.

---
+++ Services/Discount/MultiShop.Discount/Migrations/20241129154713_mig5.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiShop.Discount.Migrations
+{
+    public partial class mig5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CoupanId",
+                table: "Coupons",
+                newName: "CouponId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CouponId",
+                table: "Coupons",
+                newName: "CoupanId");
+        }
+    }
+}
