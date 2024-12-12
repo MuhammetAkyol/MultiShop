feb3220580674a4b2c9fada6319cd1eca8611a35
Muhammet Akyol <akyuzakyuzmuhammed@gmail.com>
2024-12-13 01:39:10 +0300

    ###Yorum İşlemleri 1. Comment Mikroservis  oluşturulması(gerekli nuget paketlerinin yüklenmesi commentcontext ile veritabanı local adres verilemesi) 2. Comment Migration İşlemi 3. Yorumlarda Api Testi 4. Admin Paneli Yorum Listesi ve Docker start verilmesi(Local|Portainer'den 1443 port'ta CommentDb local MsSql oluşturulması ve veri girişleri) 5. Admin Paneli Yorum Güncelleme Sayfası 6. Yorumların Ürün Detay Sayfasında Listesi 7. Yorum Veri Setleri 8. Yorumlarda Puana Göre Rating Doldurma 9. Yorum Yapma Sayfa Hazırlama 10. Yorumların Product Detail Sayfasına Taşınması 11. Yorum Yapma İşlemi ve gerekli düzenlemeler yapılarak PERFORMANS ARTTIRMA İŞLEMLERİ YAPILDI

--- Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
+++ Frontends/MultiShop.WebUI/Controllers/ProductListController.cs
@@ -1,9 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using MultiShop.DtoLayer.CommentDtos;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class ProductListController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductListController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
         public IActionResult Index(string id)
         {
             ViewBag.i = id;
@@ -13,6 +24,33 @@ namespace MultiShop.WebUI.Controllers
         public IActionResult ProductDetail(string id)
         {
             ViewBag.x = id;
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult AddComment()
+        {
+
+            return PartialView();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment(CreateCommentDto createCommentDto)
+        {
+            createCommentDto.ImageUrl = "test";
+            createCommentDto.Rating = 1;
+            createCommentDto.CreatedDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            createCommentDto.Status = false;
+            createCommentDto.ProductId = "67584e5e40008b7d58739b29";
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCommentDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7106/api/Comments", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Default");
+            }
             return View();
         }
     }
